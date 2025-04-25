2e632985fb7b598e4628be97e5ea3f5d8ab70ce9
ArunCSK <arunsubburaj@gmail.com>
2025-04-25 09:29:08 +0100

    2024 day 1

--- 2024/day_01_solution.py
+++ 2024/day_01_solution.py
@@ -17,12 +17,8 @@ def part1(input_data):
     for line in input_data.splitlines():
         left_list.append(int(line.split(" ")[0]))
         right_list.append(int(line.split("  ")[1].lstrip()))
-        # print(line)
-    # print(input_data.split(" "))
     
-    print(left_list, right_list)
     left_sorted_list , right_sorted_list = sorted(left_list), sorted(right_list)
-    print(left_sorted_list, right_sorted_list)
     result_list = 0
     for i in range(len(left_sorted_list)):
         if left_sorted_list[i] > right_sorted_list[i]:
@@ -34,7 +30,21 @@ def part1(input_data):
 
 def part2(input_data):
     # Your solution for Part 2
-    pass
+    """Solve part 2."""
+    total_points = 0
+    left_list, right_list = [], []
+    lines = input_data.split("\n")
+    for l in lines:
+        left_list.append(int(l.split(" ")[0]))
+        right_list.append(int(l.split("  ")[1].lstrip()))
+    left_list = sorted(left_list)
+    right_list = sorted(right_list)
+
+    for i in range(len(left_list)):
+        temp_count = right_list.count(left_list[i])
+        total_points += left_list[i] * temp_count
+        temp_count = 0
+    return total_points
 
 if __name__ == "__main__":
     with open("day_01_input.txt") as f:
